7f090b49bf4ddffb3cc13651d76427dc979e7770
Buyanbat.Ch <buyanbat.ch@gmail.com>
2022-07-21 00:31:01 +0800

    linked

--- src/pages/add/index.js
+++ src/pages/add/index.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { Avatar, Button } from 'antd';
 import { CameraOutlined, FormOutlined, LinkOutlined } from '@ant-design/icons';
 import { AppContext } from '../../App';
+import { Title } from '../post/view/wrapper';
 
 export default function Add() {
   const context = useContext(AppContext);
@@ -13,30 +14,37 @@ export default function Add() {
   }, []);
 
   return (
-    <div className="h-[600px] flex items-center justify-center">
-      <div className="mx-[30px] border-[1px] flex flex-col justify-around items-center h-[240px] w-[240px]">
-        <Avatar
-          icon={<FormOutlined className="text-[60px] text-caak-primary" />}
-          className="w-[90px] h-[90px] flex items-center justify-center"
-        />
-        <Button type="primary" onClick={() => navigate('/add/post')}>
-          Мэдээ оруулах
-        </Button>
+    <>
+      <Title className="text-center mt-[120px]">Төрлөө сонгоно уу?</Title>
+      <div className="h-[400px] flex items-center justify-center mb-[120px]">
+        <div className="mx-[30px] border-[1px] flex flex-col justify-around items-center h-[240px] w-[240px]">
+          <Avatar
+            icon={<FormOutlined className="text-[60px] text-caak-primary" />}
+            className="w-[90px] h-[90px] flex items-center justify-center"
+          />
+          <Button type="primary" onClick={() => navigate('/add/post')}>
+            Мэдээ оруулах
+          </Button>
+        </div>
+        <div className="mx-[30px] border-[1px] flex flex-col justify-around items-center h-[240px] w-[240px]">
+          <Avatar
+            icon={<CameraOutlined className="text-[60px] text-caak-primary" />}
+            className="w-[90px] h-[90px] flex items-center justify-center"
+          />
+          <Button type="primary" onClick={() => navigate('/add/story')}>
+            Стори оруулах
+          </Button>
+        </div>
+        <div className="mx-[30px] border-[1px] flex flex-col justify-around items-center h-[240px] w-[240px]">
+          <Avatar
+            icon={<LinkOutlined className="text-[60px] text-caak-primary" />}
+            className="w-[90px] h-[90px] flex items-center justify-center"
+          />
+          <Button type="primary" onClick={() => navigate('/add/linked')}>
+            Линкээр оруулах
+          </Button>
+        </div>
       </div>
-      <div className="mx-[30px] border-[1px] flex flex-col justify-around items-center h-[240px] w-[240px]">
-        <Avatar
-          icon={<CameraOutlined className="text-[60px] text-caak-primary" />}
-          className="w-[90px] h-[90px] flex items-center justify-center"
-        />
-        <Button type="primary">Стори оруулах</Button>
-      </div>
-      <div className="mx-[30px] border-[1px] flex flex-col justify-around items-center h-[240px] w-[240px]">
-        <Avatar
-          icon={<LinkOutlined className="text-[60px] text-caak-primary" />}
-          className="w-[90px] h-[90px] flex items-center justify-center"
-        />
-        <Button type="primary">Линкээр оруулах</Button>
-      </div>
-    </div>
+    </>
   );
 }
