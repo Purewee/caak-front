f5c5c892597605e05ee3d5f2600c8c50e07c6516
Skratta <purwee790@gmail.com>
2022-09-30 22:28:35 +0800

    last

--- src/component/header/session.js
+++ src/component/header/session.js
@@ -53,9 +53,7 @@ export default function Session() {
         <div className="w-full flex flex-row justify-between">
           <span
             onClick={() => setMobileSideMenu(!mobileSideMenu)}
-            className={`icon-fi-rs-user text-[27.5px] ${
-              mode === 'sticky' ? 'text-[#555555]' : 'text-white sm:text-[#555555]'
-            }`}
+            className={`icon-fi-rs-user text-[27.5px] ${mode === 'sticky' ? 'text-[#555555]' : 'text-caak-black'}`}
           />
         </div>
         {mobileSideMenu && (
