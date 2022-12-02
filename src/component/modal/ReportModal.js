fef49c0445f0a3d4b48f17ed91034a0d6b99e262
Skratta <purwee790@gmail.com>
2022-12-02 17:12:14 +0800

    asd

--- src/component/modal/ReportModal.js
+++ src/component/modal/ReportModal.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { message, Modal, Radio, List, Button } from 'antd';
 import { gql, useQuery, useMutation } from '@apollo/client';
 import { useAuth } from '../../context/AuthContext';
+import useMediaQuery from '../navigation/useMediaQuery';
 
 const REPORT_TYPES = gql`
   query getReportTypes {
@@ -25,28 +26,37 @@ export default function ReportModal({ post, toggle }) {
   const [sendReport, { loading: sending }] = useMutation(SEND_REPORT, { variables: { articleId: post.id } });
   const reportTypes = data?.reportTypes || [];
 
+  const isMobile = useMediaQuery('screen and (max-width: 600px)');
+
   useEffect(() => {
     setTypeId(reportTypes[0]?.id);
   }, [reportTypes]);
 
   return (
     <Modal
+      width={600}
       visible
-      width={480}
       confirmLoading={sending || loading}
       bodyStyle={{ padding: 0 }}
       closable={false}
       footer={false}
-      title={<p className="text-[26px] font-condensed font-bold leading-[32px]">Мэдээг репортлох</p>}
+      title={<p className="text-[26px] font-condensed font-bold leading-[32px] border-b-0">Мэдээг репортлох</p>}
     >
       <div>
         <List
-          grid={{ column: 2, gutter: 0 }}
-          style={{ paddingInline: 20 }}
+          grid={{ column: isMobile ? 1 : 2, gutter: 0 }}
+          style={{ paddingInline: 23 }}
           dataSource={reportTypes}
-          renderItem={(x) => {
+          renderItem={(x, index) => {
             return (
-              <div className="py-[17px]" key={x.id}>
+              <div
+                className={`py-[17px] border-b border-[#D4D8D8] ${
+                  index === 1 && 'sm:border-l border-[#D4D8D8] sm:pl-[14px]'
+                } ${index === 3 && 'sm:border-l border-[#D4D8D8] sm:pl-[14px]'} ${
+                  index === 5 && 'sm:border-l border-[#D4D8D8] sm:pl-[14px]'
+                }`}
+                key={x.id}
+              >
                 <Radio
                   id={x.id}
                   onChange={(e) => {
@@ -65,7 +75,7 @@ export default function ReportModal({ post, toggle }) {
             );
           }}
         />
-        <div className="border-t py-[18px] flex flex-row items-center justify-end pr-[24px]">
+        <div className="py-[18px] flex flex-row items-center justify-end pr-[24px]">
           <Button
             onClick={toggle}
             className="w-[76px] h-[34px] border border-[#D4D8D8] hover:border-[#D4D8D8] rounded-[4px] text-[15px] text-caak-black hover:text-caak-black font-medium font-roboto"
