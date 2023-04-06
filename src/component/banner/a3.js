63960740ffb80a02dd085ba8e58d2a00dd6c2e9c
Skratta <purwee790@gmail.com>
2023-04-06 21:31:28 +0800

    A3 fix

--- src/component/banner/a3.js
+++ src/component/banner/a3.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { imagePath } from '../../utility/Util';
 import useMediaQuery from '../navigation/useMediaQuery';
+import { Link } from 'react-router-dom';
 
 export default function A3({ banner }) {
   const isMobile = useMediaQuery(640);
@@ -11,12 +12,16 @@ export default function A3({ banner }) {
 
   return (
     <div className="w-full">
-      <p className="text-[#BBBEBE] text-[13px] py-[9px] leading-[15px]">СУРТАЛЧИЛГАА</p>
-      <img src={imagePath(banner?.mobileFileUrl)} alt={banner?.title} className="w-full object-cover" />
-      <div className="flex w-full justify-end py-[10px] items-center">
-        <span className="icon-fi-rs-megaphone text-[16px] mr-1" />
-        <p className="text-[#BBBEBE] text-[15px] leading-[18px]">Caak Ads</p>
-      </div>
+      <p className="text-[#BBBEBE] md:text-[13px] text-[11px] py-[9px] leading-[15px] ">СУРТАЛЧИЛГАА</p>
+      <a href={banner.url} target="_blank">
+        <img src={imagePath(banner?.mobileFileUrl)} alt={banner?.title} className="w-full object-cover" />
+      </a>
+      <Link to="/help/1">
+        <div className="flex w-full justify-end py-[10px] items-center">
+          <span className="icon-fi-rs-megaphone text-[16px] mr-1" />
+          <p className="text-[#BBBEBE] text-[15px] leading-[18px]">Caak Ads</p>
+        </div>
+      </Link>
       {/* <p className="text-[#555555] text-[18px] leading-[30px]">
         Туйлын туяан гэрлийн доор буга болон хаски нохойн чарга хөлөглөн мартагдашгүй дурсамжаа бүтээх алхмаа яг одоо
         хийгээрэй.
